4a3cca09c9c725079a50c7fab300b74019081da0
Davi Alves <alves.pindoba@gmail.com>
2019-04-14 17:29:53 -0300

    pqp

--- index.js
+++ index.js
@@ -11,6 +11,14 @@ const dbConnection = sqlite.open(path.resolve(__dirname, 'banco.sqlite'), {
 
 const port = process.env.PORT || 3000
 
+app.use('/admin', (request, response, next) => {
+    if(request.hostname === 'localhost') {
+        next()
+    } else {
+        response.send('Not Allowed')
+    }
+})
+
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 app.use(express.static(path.join(__dirname, 'public')))
